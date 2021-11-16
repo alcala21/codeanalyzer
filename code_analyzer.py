1a94fdb78df78013152a48bb244c96bd813c31d7
Carlos Alcala <alcala21@gmail.com>
2021-11-15 21:31:13 -0600

    Stage 4/5. Static Code Analyzer.

--- code_analyzer.py
+++ code_analyzer.py
@@ -40,6 +40,36 @@ def s006(_res, _count, _fpath):
     _res += f"{_fpath}: Line {_count}: S006 More than two blank lines preceding a code line",
 
 
+def strip_function_or_class(construction, _line):
+    match = re.search(construction, _line)
+    if match:
+        _line = _line[match.span()[1]:]
+    return match, _line
+
+
+def s007(_res, _count, _line, _fpath):
+    match, _line = strip_function_or_class("(class|def)", _line)
+    if match:
+        if re.match(r"  +", _line):
+            _res += f"{_fpath}: Line {_count}: S007 Too many spaces after construction_name",
+
+
+def s008(_res, _count, _line, _fpath):
+    match, _line = strip_function_or_class("class", _line)
+    _line = _line.strip()
+    if match:
+        if not re.match(r"[A-Z][a-z]+[A-z0-9]*", _line):
+            _res += f"{_fpath}: Line {_count}: S008 Class name class_name should be written in CamelCase",
+
+
+def s009(_res, _count, _line, _fpath):
+    match, _line = strip_function_or_class("def", _line)
+    _line = _line.strip()
+    if match:
+        if not re.match(r"(_{,2}[a-z]+_{,2})+", _line):
+            _res += f"{_fpath}: Line {_count}: S009 Function name function_name should be written in snake_case.",
+
+
 def analyzer(_res, _file):
     count = 0
     with open(_file, 'r') as f:
@@ -54,9 +84,11 @@ def analyzer(_res, _file):
                 s003(_res, count, line, _file)
                 s004(_res, count, line, _file)
                 s005(_res, count, line, _file)
-
                 if empty > 2:
                     s006(_res, count, _file)
+                s007(_res, count, line, _file)
+                s008(_res, count, line, _file)
+                s009(_res, count, line, _file)
 
                 empty = 0
 
@@ -70,7 +102,6 @@ if os.path.isdir(arg):
             analyzer(res, arg + "/" + file)
 elif os.path.isfile(arg):
     analyzer(res, arg)
-
 
 for x in res:
     print(x)
